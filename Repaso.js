a98b7dc7a5970419fd66d71506d465be1b5b13ec
xd <nose>
2022-03-23 10:12:26 -0500

    RrRRRR

---
+++ Repaso.js
@@ -0,0 +1,34 @@
+//Primer Punto
+//Creacion de variables 
+
+var Metros;
+var Kilometros
+
+//Solicitar Dtos al usuario
+
+Metros = prompt("Por favor ingrese el valor en metros");
+
+//Realizar la operacion
+
+Kilometros = Metros / 1000;
+
+//Mostrar mensaje con resultado
+
+console.log (Metros + " metros equivalen a " + Kilometros + " km ") ;
+
+//Punto 2
+
+var combustible
+var kilometross
+
+kilometross = prompt ("Por favor ingrese la cantidad de kilometros que recorrio el avion");
+ combustible = (kilometross/100)*290 ;
+
+ console.log(" El avion gasto " + combustible + " litros de combustible al recorrer " + kilometross + " km ")
+
+ //Punto 3 
+
+ var kilolitros
+
+ kilolitros = kilometross/1000
+ console.log(" Los kilolitros son " + kilolitros)
---
+++ Taller3.js
@@ -0,0 +1,60 @@
+//Taller 3
+//David Peña
+//Punto 1  
+
+var litors
+var tiempo
+
+litors = prompt (" Ingrese los litros que es capaz de almacenar su piscina ");
+tiempo = litors/145;
+console.log(" Su piscina tardara en llenarse " + tiempo + " horas");
+
+//Punto 2
+
+var Metross
+var milimetros
+ 
+Metross = prompt ("Ingrese el numero de metros");
+milimetros = Metross*1000
+console.log(" Los milimetros que tiene su metro son " + milimetros)
+
+//Punto 3
+
+var decimetros
+
+decimetros = Metross * 10
+console.log(" Los decimetros que tiene su metro son " + decimetros)
+
+//Punto 4
+
+var kilometross
+
+kilometross = decimetros * 10000
+console.log(" Los kilometros son que tiene su decimetro son " + kilometross)
+
+//Punto 5
+
+var minutos
+var horas
+var precio
+
+horas = prompt("ingrese las horas que estuvo en llamada")
+minutos = 60/horas
+precio = minutos * 120
+console.log(" El precio de su llamada es " + precio)
+
+//Punto 6
+var hora1
+var valorclase
+var horasextras
+var resta
+hora1 = 40000
+horasextras = prompt("Ingrese las horas haya tomado")
+resta = horasextras - 1
+valorclase = hora1 + (resta*25000) 
+console.log(" El costo de sus clases es " + valorclase)
+
+
+
+
+ 
